a596a2177bb9b46d85d06af87c4ac057bb384876
austinhoag <austinthomashoag@gmail.com>
2019-09-10 11:00:57 -0700

    improve visual appeal of ontology nodes

--- lightserv/ontology/utils.py
+++ lightserv/ontology/utils.py
@@ -20,76 +20,14 @@ my_test_graph.attr('node',shape='box')
 
 my_graph = graphviz.Digraph(format='svg',strict=True) # strict means you cant have more than 1 edge between nodes
 my_graph.attr('node',shape='box')
-root_body_str = '\troot [label=<<TABLE><TR><TD href="/interactive_ontology?input_nodename=root">root</TD><TD href="/interactive_ontology?input_nodename=root&amp;contract=True">-</TD></TR></TABLE>>]'
 
-
-def test_expand_graph(dic=test_ontology_dict,graph=my_test_graph,input_nodename='root'):
-    """ 
-    ---PURPOSE---
-    Take an existing graph and add to it the children 
-    of an input_nodename. Recursive function.
-    ---INPUT---
-    dic             Dictionary representing the entire ontology graph (with "rank" key included)
-    graph           The graphviz graph object that will be updated.
-                    graph may be a subgraph of the entire graph as long as 
-                    input_nodename is a node name in it
-    input_nodename  The name of the node whose children you want to display
-    """
-    name = dic.get('name')
-    children = dic.get('children')
-    if input_nodename == None: # default is root
-        graph.node('root',href=url_for('ontology.test_ontology_int',nodename='root'))
-    if name == input_nodename:
-        for child in children: # child is a dict
-            child_name = child.get('name')
-            # body_str = '\t'
-            # graph.node(name,href=url_for('ontology.test_ontology_int',nodename=name))
-            graph.node(child_name,href=url_for('ontology.test_ontology_int',nodename=child_name))
-            graph.edge(name,child_name)
-        return graph
-     
-    for child in children:
-        test_expand_graph(child,graph,input_nodename=input_nodename)
-    return graph
-
-# def expand_graph(dic=ontology_dict,graph=my_graph,input_nodename='root'):
-#     """ 
-#     ---PURPOSE---
-#     Take an existing graph and add to it the children 
-#     of an input_nodename. Recursive function.
-#     ---INPUT---
-#     dic             Dictionary representing the entire ontology graph (with "rank" key included)
-#     graph           The graphviz graph object that will be updated.
-#                     graph may be a subgraph of the entire graph as long as 
-#                     input_nodename is a node name in it
-#     input_nodename  The name of the node whose children you want to display
-#     """
-#     name = dic.get('name')
-#     children = dic.get('children')
-#     if input_nodename == None: # default is root
-#         href_expand = url_for('ontology.interactive_ontology',nodename='root')
-#         href_contract = url_for('ontology.interactive_ontology',nodename='root',contract=True).replace('&','&amp;')
-#         # href_contract = '/interactive_ontology?contract=True&amp;nodename=root'
-#         graph.node('root',label='''<<TABLE><TR><TD href="{}">root</TD><TD href="{}">-</TD></TR></TABLE>>'''.format(href_expand,href_contract))
-#     if name == input_nodename:
-#         for child in children: # child is a dict
-#             child_name = child.get('name')
-#             # body_str = '\t'
-#             # graph.node(name,href=url_for('ontology.interactive_ontology',nodename=name))
-#             # graph.node(child_name,href=url_for('ontology.interactive_ontology',nodename=child_name))
-#             href_expand = url_for('ontology.interactive_ontology',nodename=child_name)
-#             href_contract = url_for('ontology.interactive_ontology',nodename=child_name,contract=True).replace('&','&amp;')
-#             # href_contract = url_for('ontology.interactive_ontology',nodename=child_name,contract=True).replace('&','&amp;')
-
-#             # href_contract = 
-
-#             graph.node(child_name,label='''<<TABLE><TR><TD href="{}">{}</TD><TD href="{}">-</TD></TR></TABLE>>'''.format(href_expand,child_name,href_contract))
-#             graph.edge(name,child_name)
-#         return graph
-     
-#     for child in children:
-#         expand_graph(child,graph,input_nodename=input_nodename)
-#     return graph
+table_border = 0
+contract_cell_border=1
+root_body_str = f'\troot [label=<<TABLE BORDER="{0}">\
+<TR>\
+<TD href="/interactive_ontology?input_nodename=root">root</TD>\
+<TD BORDER="{1}" href="/interactive_ontology?input_nodename=root&amp;contract=True">-</TD>\
+</TR></TABLE>>]'.format(table_border,contract_cell_border)
 
 def expand_graph(dic=ontology_dict,graph=my_graph,input_nodename='root'):
     """ 
@@ -109,14 +47,17 @@ def expand_graph(dic=ontology_dict,graph=my_graph,input_nodename='root'):
 #         print("here")
         href_expand = '/interactive_ontology?input_nodename=root'
         href_contract = '/interactive_ontology?input_nodename=root&amp;contract=True'
-        graph.node('root',label='''<<TABLE><TR><TD href="{}">root</TD><TD href="{}">-</TD></TR></TABLE>>'''.format(href_expand,href_contract))
+        graph.node('root',label='''<<TABLE BORDER="{0}"><TR><TD href="{1}">root</TD>\
+<TD BORDER="{2}" href="{3}">-</TD></TR></TABLE>>'''\
+            .format(table_border,href_expand,contract_cell_border,href_contract))
     if name == input_nodename:
         for child in children: # child is a dict
             child_name = child.get('name')
             href_expand = '/interactive_ontology?input_nodename={}'.format(child_name)
             href_contract = '/interactive_ontology?input_nodename={}&amp;contract=True'.format(child_name)
-            graph.node(child_name,label='''<<TABLE><TR><TD href="{0}">{1}</TD><TD href="{2}">-</TD></TR></TABLE>>'''\
-                       .format(href_expand,child_name,href_contract))
+            graph.node(child_name,label='''<<TABLE BORDER="{0}"><TR><TD href="{1}">{2}</TD>\
+<TD BORDER="{3}" href="{4}">-</TD></TR></TABLE>>'''\
+                       .format(table_border,href_expand,child_name,contract_cell_border,href_contract))
             graph.edge(name,child_name)
         return graph
      
@@ -145,8 +86,8 @@ def contract_graph(dic=ontology_dict,graph=my_graph,input_nodename='root'):
             child_label = f'"{child_name}"' if len(child_name.split())>1 else child_name
             edge_str = f'\t{name_label} -> {child_label}' 
 #             print(edge_st/r)
-            child_body_str = '\t{0} [label=<<TABLE><TR><TD href="/interactive_ontology?input_nodename={1}">{1}</TD>\
-<TD href="/interactive_ontology?input_nodename={1}&amp;contract=True">-</TD></TR></TABLE>>]'.format(child_label,child_name)
+            child_body_str = '\t{0} [label=<<TABLE BORDER="{1}"><TR><TD href="/interactive_ontology?input_nodename={2}">{2}</TD>\
+<TD BORDER="{3}" href="/interactive_ontology?input_nodename={2}&amp;contract=True">-</TD></TR></TABLE>>]'.format(child_label,table_border,child_name,contract_cell_border)
 
             if edge_str in graph.body:
                 del graph.body[graph.body.index(edge_str)]
