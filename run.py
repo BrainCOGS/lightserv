c42b7b193f02a16e6edfe7fd24a3750906a2c5b8
austinhoag <austinthomashoag@gmail.com>
2019-09-23 21:42:54 -0400

    add ability to run from braincogs00 and elsewhere

--- run.py
+++ run.py
@@ -2,9 +2,13 @@ import os
 os.environ['FLASK_MODE']='DEV'
 
 from lightserv import create_app
-
+import socket
 
 app = create_app()
 
+
 if __name__ == '__main__':
-	app.run(debug=True)
+	if socket.gethostname() == 'braincogs00.pni.princeton.edu':
+		app.run(host='0.0.0.0',port='8080',debug=True)
+	else:
+		app.run(debug=True)
