3e65b9525155d05be0f7334747c27c0488ea4540
austinhoag <austinthomashoag@gmail.com>
2019-09-26 17:09:32 -0400

    Make iDISCO+ clearing form with interactive updating

--- lightserv/clearing/routes.py
+++ lightserv/clearing/routes.py
@@ -11,6 +11,7 @@ from lightserv import db
 from .utils import determine_clearing_form
 # import cloudvolume
 import numpy as np
+import datajoint as dj
 
 clearing = Blueprint('clearing',__name__)
 
@@ -30,3 +31,43 @@ def update_clearing(experiment_id):
 	# 	form.notes.data = clearing_contents.fetch1('clearing_notes')
 
 	return render_template('clearing.html',form=form,clearing_table=clearing_table,clearing_protocol=clearing_protocol,experiment_id=experiment_id)
+
+
+@clearing.route("/clearing/idiscoplus_entry/<experiment_id>",methods=['GET','POST'])
+def make_iDISCOplus_entry(experiment_id): 
+	from wtforms.fields.html5 import DateField
+	exp_contents = db.Experiment() & f'experiment_id="{experiment_id}"'
+	assert exp_contents, f"experiment_id={experiment_id} does not exist.\
+						   It must exist before the clearing for this experiment can be done"
+	clearing_table = ClearingTable(exp_contents)
+	clearing_protocol = exp_contents.fetch1('clearing_protocol')
+	form = determine_clearing_form(clearing_protocol,existing_form=request.form)
+
+	''' Check to see if there is an entry in the db yet. If not create one with all NULL values.
+	They will get updated as the user fills out the form '''
+	clearing_contents = db.IdiscoPlusClearing() & f'experiment_id="{experiment_id}"'
+	if not clearing_contents:
+		experiment_username = exp_contents.fetch1('username')
+		insert_dict = {'experiment_id':experiment_id,
+						'username':experiment_username,'clearer':session['user']}
+		db.IdiscoPlusClearing().insert1(insert_dict)
+		print("Created clearing database entry")
+
+	''' Handle user's post requests '''
+	if request.method == 'POST':
+		submit_keys = [x for x in form._fields.keys() if 'submit' in x]
+		for key in submit_keys:
+			if form[key].data:
+				if key == 'submit': # The final submit button
+					print("Submitting entire form")
+				else: 
+					''' Update the row '''
+					column_name = key.rstrip('_submit')
+					clearing_entry_dict = clearing_contents.fetch1() # returns as a dict
+					clearing_entry_dict[column_name]=form[column_name].data
+					clearing_contents.delete_quick()
+					db.IdiscoPlusClearing().insert1(clearing_entry_dict)
+					print(f"updated row for value: {column_name}")
+
+
+	return render_template('clearing/idiscoplus.html',form=form,clearing_table=clearing_table,experiment_id=experiment_id)
--- lightserv/clearing/utils.py
+++ lightserv/clearing/utils.py
@@ -1,15 +1,15 @@
 from lightserv.clearing.forms import (iDiscoPlusImmunoForm, iDiscoAbbreviatedForm,
 									  uDiscoForm, iDiscoPlusForm, iDiscoEduForm )
 
-def determine_clearing_form(clearing_protocol):
+def determine_clearing_form(clearing_protocol,existing_form):
 	if clearing_protocol == 'iDISCO+': 
-		form = iDiscoPlusForm()
+		form = iDiscoPlusForm(existing_form)
 	elif clearing_protocol == 'iDISCO abbreviated clearing':
-		form = iDiscoAbbreviatedForm()
+		form = iDiscoAbbreviatedForm(existing_form)
 	elif clearing_protocol == 'uDISCO':
-		form = uDiscoForm()
+		form = uDiscoForm(existing_form)
 	elif clearing_protocol == 'iDISCO+_immuno':
-		form = iDiscoPlusImmunoForm()
+		form = iDiscoPlusImmunoForm(existing_form)
 	elif clearing_protocol == 'iDISCO_EdU':
 		form = iDiscoEduForm()
 	return form
