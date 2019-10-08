cd8aa6e5bf85e5fdb5a751a20dd04541325cb8e4
austinhoag <austinthomashoag@gmail.com>
2019-10-08 10:25:19 -0400

    add javascript folder with first few scripts

---
+++ lightserv/static/javascript/ontology_scroller.js
@@ -0,0 +1,4 @@
+var window_height = $(window).height()
+var window_width = $(window).width()
+
+window.scrollTo({ top: window_height, left: window_width, behavior: 'smooth' });
---
+++ lightserv/static/javascript/tableswapper.js
@@ -0,0 +1,24 @@
+
+$("#tableSwapper").click(function(){
+$("table").each(function() {
+    var $this = $(this);
+    var newrows = [];
+    $this.find("tr").each(function(){
+        var i = 0;
+        $(this).find("td, th").each(function(){
+            i++;
+            if(newrows[i] === undefined) { newrows[i] = $("<tr></tr>"); }
+            if(i == 1)
+                newrows[i].append("<th>" + this.innerHTML  + "</th>");
+            else
+                newrows[i].append("<td>" + this.innerHTML  + "</td>");
+        });
+    });
+    $this.find("tr").remove();
+    $.each(newrows, function(){
+        $this.append(this);
+    });
+});
+
+return false;
+});
