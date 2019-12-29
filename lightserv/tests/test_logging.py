7a04615c31e0b319886fe297baf30f7f60f4bf77
Austin Hoag <austinthomashoag@gmail.com>
2019-12-29 12:27:15 -0500

    fix tests to work with new clearingbatch db structure; found bug in new request form while testing

--- lightserv/tests/test_logging.py
+++ lightserv/tests/test_logging.py
@@ -4,25 +4,18 @@ import tempfile,webbrowser
 from lightserv import db_lightsheet
 from datetime import datetime
 
-# def test_exps_show_up_on_main_page(test_client,test_login):
-# 	""" Check that the test post is rendered on the home page """
-# 	response = test_client.get(url_for('main.home'), follow_redirects=True)
-
-# 	assert b'light sheet requests' in response.data and b'rabbit anti-RFP 1:1000' in response.data
-
-
 def test_GET_log_entry(test_client,test_login):
 	""" Ensure that when the user issues a GET request
-	to the home page a log entry is inserted into the Log() table """
+	to the home page a log entry is inserted into the user action log table """
 	response = test_client.get(url_for('requests.all_requests'),
 		follow_redirects=True)
 	log_event = db_lightsheet.UserActionLog().fetch()[-1]['event']
 	assert log_event == '''ahoag GET request to route: "all_requests()" in lightserv.requests.routes'''
 	# assert b'Background Info' in response.data and b"Clearing setup" not in response.data
 
-def test_GET_log_entry_other_user(test_client,test_login_nonadmin):
+def test_GET_log_entry_nonadmin_user(test_client,test_login_nonadmin):
 	""" Ensure that when a user other than ahoag issues a GET request
-	to the home page a log entry is inserted into the Log() table
+	to the home page a log entry is inserted into the user action log table
 	under their name. """
 	response = test_client.get(url_for('requests.all_requests'),
 		follow_redirects=True)
