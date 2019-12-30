e7ffea179a3c55ec90bc2fd8c2e32941cafc782b
Austin Hoag <austinthomashoag@gmail.com>
2019-12-30 17:32:35 -0500

    fix issue where if you recently ran pytestflask then FLASK_MODE would be set to TEST

--- setup_schema.py
+++ setup_schema.py
@@ -1,11 +1,11 @@
 # import datajoint as dj
-
+import os
 # dj.config['database.host'] = '127.0.0.1'
 # dj.config['database.port'] = 3306
 
 # dj.config['database.user'] = 'ahoag'
 # dj.config['database.password'] = 'p@sswd'
-
+os.environ['FLASK_MODE'] = 'DEV'
 from schemas import lightsheet
 from schemas import admin
 # from schemas import microscope
