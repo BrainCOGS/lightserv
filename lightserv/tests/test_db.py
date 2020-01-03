a0638c3b07c4da9c80ba4f67ed233fe4091b29b7
Austin Hoag <austinthomashoag@gmail.com>
2020-01-03 14:26:39 -0500

    add test to make sure login fixtures insert to the db

--- lightserv/tests/test_db.py
+++ lightserv/tests/test_db.py
@@ -1,15 +1,20 @@
+from lightserv import db_lightsheet
 
-def test_tables_exist(test_client,test_schema):
-	user_contents = test_schema.User()
-	request_contents = test_schema.Experiment()
+# def test_tables_exist(test_client,test_schema):
+# 	user_contents = test_schema.User()
+# 	request_contents = test_schema.Experiment()
+# 	print(user_contents)
+# 	assert len(user_contents) > 0 
+# 	assert len(request_contents) == 33
+
+# def test_User_table_ingested_correctly(test_client,test_schema):
+# 	user_contents = test_schema.User()
+# 	usernames,emails = user_contents.fetch('username','princeton_email')
+# 	assert 'jverpeut' in usernames and 'tomohito' in usernames
+# 	assert 'jverpeut@princeton.edu' in emails and 'zhihaoz@princeton.edu' in emails
+
+def test_login_inserts_user(test_client,test_login):
+	user_contents = db_lightsheet.User()
 	print(user_contents)
 	assert len(user_contents) > 0 
-	assert len(request_contents) == 33
-
-def test_User_table_ingested_correctly(test_client,test_schema):
-	user_contents = test_schema.User()
-	usernames,emails = user_contents.fetch('username','princeton_email')
-	assert 'jverpeut' in usernames and 'tomohito' in usernames
-	assert 'jverpeut@princeton.edu' in emails and 'zhihaoz@princeton.edu' in emails
-
 
