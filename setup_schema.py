9e43d67c2278d48f8ec672430e482a20b84d1444
Austin Hoag <austinthomashoag@gmail.com>
2020-04-01 08:36:15 -0400

    better documentation for schema imports/drops

--- setup_schema.py
+++ setup_schema.py
@@ -6,24 +6,23 @@ dj.config['database.port'] = 3306
 
 dj.config['database.user'] = os.environ['DJ_DB_USER']
 dj.config['database.password'] = os.environ['DJ_DB_PASS']
-# dj.config['database.host'] = '127.0.0.1'
-# dj.config['database.port'] = 3306
 
-# dj.config['database.user'] = 'ahoag'
-# dj.config['database.password'] = 'p@sswd'
 os.environ['FLASK_MODE'] = 'DEV'
 
 spockadmin_schema = dj.schema('ahoag_spockadmin_demo')
 lightsheet_schema = dj.schema('ahoag_lightsheet_demo')
 admin_schema = dj.schema('ahoag_admin_demo')
-# microscope_schema = dj.schema('ahoag_microscope_demo')
+microscope_schema = dj.schema('ahoag_microscope_demo')
 
 drop_lightsheet_admin = input("Drop light sheet, admin and spockadmin schemas? (yes or No): ")
 if drop_lightsheet_admin == 'yes':
+	# You have to drop the schemas that use the other schemas first
+	# because if you try to drop a parent schema but a dependent schema
+	# still exists then you will get a foreign key error
 	admin_schema.drop(force=True)
 	lightsheet_schema.drop(force=True)
 	spockadmin_schema.drop(force=True)
-# # from schemas import admin # admin imports lightsheet so no need to reimport here
+
 from schemas import spockadmin 
 from schemas import lightsheet 
 from schemas import admin
@@ -31,5 +30,5 @@ from schemas import admin
 # if drop_microscope == 'yes':
 # 	microscope_schema.drop(force=True)
 	
-# from schemas import microscope
+from schemas import microscope
 
