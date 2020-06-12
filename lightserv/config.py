b86c5daeb5a6821024324ee7a96ad6edbdbe43d8
Austin Hoag <austinthomashoag@gmail.com>
2020-06-12 09:13:31 -0400

    lengthen ng viewer health checker timeout to 12 hours

--- lightserv/config.py
+++ lightserv/config.py
@@ -95,7 +95,7 @@ class ProdConfig(BaseConfig):
 	CLEARING_CALENDAR_ID = os.environ.get("CLEARING_CALENDAR_URL") # the test calendar for the time being
 	SECRET_KEY = os.environ.get('SECRET_KEY')
 	DATA_BUCKET_ROOTPATH = '/jukebox/LightSheetData/lightserv'
-	NG_VIEWER_EXPIRE_SECONDS = 21600 # 6 hours - time that a neuroglancer viewer and its cloudvolumes are allowed to stay up 
+	NG_VIEWER_EXPIRE_SECONDS = 43200 # 6 hours - time that a neuroglancer viewer and its cloudvolumes are allowed to stay up 
 	SPOCK_LSADMIN_USERNAME = 'lightserv-test'
 
 	CELERYBEAT_SCHEDULE = {
@@ -117,7 +117,7 @@ class ProdConfig(BaseConfig):
 		},
 		'ng_viewer_cleanser': {
 		'task': 'lightserv.neuroglancer.tasks.ng_viewer_checker',
-		'schedule': timedelta(minutes=5)
+		'schedule': timedelta(minutes=30)
 		},
 		'rawprecomp_job_status_checker': {
 		'task': 'lightserv.imaging.tasks.check_raw_precomputed_statuses',
