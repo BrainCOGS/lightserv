6c02f91c479c30fec2c1cdb8dee686e37d341fba
Austin Hoag <austinthomashoag@gmail.com>
2021-10-05 09:48:39 -0400

    remove cells from kelly cfos ng example

--- lightserv/neuroglancer/routes.py
+++ lightserv/neuroglancer/routes.py
@@ -3574,30 +3574,30 @@ def kelly_cfos_smartspim_demo():
     proxy_h.addroute(proxypath=proxypath,proxytarget=f"http://{cv_container_name}:1337")
 
     # """ CV 3: Raw cells """
-    layer_type = "annotation"
+    # layer_type = "annotation"
                
-    cv_number += 1              
-    cv_container_name = f'{session_name}_rawcells_{brain_short}'
-    cv_name = f"rawcells_{brain_short}"
-    cv_path = os.path.join(layer_rootdir,'points')      
-    """ send the data to the viewer-launcher
-    to launch the cloudvolume """                       
-    cv_dict = dict(cv_path=cv_path,cv_name=cv_name,
-        cv_container_name=cv_container_name,
-        layer_type=layer_type,session_name=session_name)
-    requests.post('http://viewer-launcher:5005/corslauncher',json=cv_dict)
-    logger.debug("Made post request to viewer-launcher to launch cloudvolume")
+    # cv_number += 1              
+    # cv_container_name = f'{session_name}_rawcells_{brain_short}'
+    # cv_name = f"rawcells_{brain_short}"
+    # cv_path = os.path.join(layer_rootdir,'points')      
+    # """ send the data to the viewer-launcher
+    # to launch the cloudvolume """                       
+    # cv_dict = dict(cv_path=cv_path,cv_name=cv_name,
+    #     cv_container_name=cv_container_name,
+    #     layer_type=layer_type,session_name=session_name)
+    # requests.post('http://viewer-launcher:5005/corslauncher',json=cv_dict)
+    # logger.debug("Made post request to viewer-launcher to launch cloudvolume")
 
-    """ Enter the cv information into redis
-    so I can get it from within the neuroglancer container """
-    kv.hmset(session_name, {f"cv{cv_number}_container_name": cv_container_name,
-        f"cv{cv_number}_name": cv_name, f"layer{cv_number}_type":layer_type})
-    # increment the number of cloudvolumes so it is up to date
-    kv.hincrby(session_name,'cv_count',1)
-    # register with the confproxy so that it can be seen from outside the nglancer network
-    proxy_h = pp.progproxy(target_hname='confproxy')
-    proxypath = os.path.join('cloudvols',session_name,cv_name)
-    proxy_h.addroute(proxypath=proxypath,proxytarget=f"http://{cv_container_name}:8080")
+    # """ Enter the cv information into redis
+    # so I can get it from within the neuroglancer container """
+    # kv.hmset(session_name, {f"cv{cv_number}_container_name": cv_container_name,
+    #     f"cv{cv_number}_name": cv_name, f"layer{cv_number}_type":layer_type})
+    # # increment the number of cloudvolumes so it is up to date
+    # kv.hincrby(session_name,'cv_count',1)
+    # # register with the confproxy so that it can be seen from outside the nglancer network
+    # proxy_h = pp.progproxy(target_hname='confproxy')
+    # proxypath = os.path.join('cloudvols',session_name,cv_name)
+    # proxy_h.addroute(proxypath=proxypath,proxytarget=f"http://{cv_container_name}:8080")
 
     """ Neuroglancer viewer container """
     ng_container_name = f'{session_name}_ng_container'
